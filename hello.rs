3cc85e11c6288a5844dbdc45669102ad189c6d7c
Denis Lee <denislee@gmail.com>
2019-07-27 22:38:39 -0300

    added files to a simple api

---
+++ hello.rs
@@ -0,0 +1,14 @@
+use f
+
+fn main() {
+    let company_string = "string";
+    let rating_float = 4.5;
+    let is_growing_boolean = true;
+    let icon_char = '♥';
+
+    println!("test");
+    println!("string {}", company_string);
+    println!("rating_float {}", rating_float);
+    println!("is_growing_boolean {}", is_growing_boolean);
+    println!("icon_chat {}", icon_char);
+}
